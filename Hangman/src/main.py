41110d72bb4638ba51ae604531e330ddb60c9259
BC30138 <sasori.axele@gmail.com>
2019-08-05 17:05:10 +0300

    __pycache__ deleted

--- Hangman/src/main.py
+++ Hangman/src/main.py
@@ -4,6 +4,12 @@ import pickle
 with open('data/words.dictionary', 'rb') as load_obj_file:
     WORDS_LOAD = pickle.load(load_obj_file)
 
+def choose_mode():
+    while True:
+        choosen_mode = input()
+        if choosen_mode == 'PvP':
+            mode = 'PvP'
+
 print('''Welcome to
 H A N G M A N
 This is awesome game, just belive''')
